d634c8abb347cee5b135fc956769436b7c6d7a90
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-06 20:47:18 +0700

    chore: add endpoint provider

---
+++ app/src/main/java/com/ichwan/rest/quranapi/data/ChapterService.kt
@@ -0,0 +1,15 @@
+package com.ichwan.rest.quranapi.data
+
+import com.ichwan.rest.quranapi.models.ChapterResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ChapterService {
+
+    @GET("chapter")
+    suspend fun getChapters(
+        @Query("language") language: String?
+    ): Response<ChapterResponse>
+
+}
