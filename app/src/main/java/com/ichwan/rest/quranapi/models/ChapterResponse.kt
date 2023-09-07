b30020069c612a198566f3d831d8dd7295a8f8de
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-07 15:12:50 +0700

    fixed!: change to single file

--- app/src/main/java/com/ichwan/rest/quranapi/models/ChapterResponse.kt
+++ app/src/main/java/com/ichwan/rest/quranapi/models/ChapterResponse.kt
@@ -10,3 +10,34 @@ data class ChapterResponse(
 	@field:SerializedName("chapters")
 	val chapters: MutableList<ChaptersItem>
 ) : Parcelable
+
+@Parcelize
+data class ChaptersItem(
+
+	@field:SerializedName("translated_name")
+	val translatedName: TranslatedName? = null,
+
+	@field:SerializedName("revelation_order")
+	val revelationOrder: Int? = null,
+
+	@field:SerializedName("verses_count")
+	val versesCount: Int? = null,
+
+	@field:SerializedName("id")
+	val id: Int? = null,
+
+	@field:SerializedName("name_simple")
+	val nameSimple: String? = null,
+
+	@field:SerializedName("revelation_place")
+	val revelationPlace: String? = null
+) : Parcelable
+
+
+@Parcelize
+data class TranslatedName(
+
+	@field:SerializedName("name")
+	val name: String? = null,
+
+	) : Parcelable
