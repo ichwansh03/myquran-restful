47eab1fb752455dcaf5855ebc9a48bcef438126e
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-07 10:34:38 +0700

    fixed!: initialized binding

--- app/src/main/java/com/ichwan/rest/quranapi/MainActivity.kt
+++ app/src/main/java/com/ichwan/rest/quranapi/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         viewModel.getChapter()
