11172bea20e8d69cc893b11fa9791ee6e916c562
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-08 08:53:53 +0700

    chore: add login starter project

---
+++ app/src/main/java/com/ichwan/rest/quranapi/auth/presentation/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.ichwan.rest.quranapi.auth.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ichwan.rest.quranapi.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
