b3c79ee51413d5197a94ea9b18ed461d8bc90325
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-10 11:31:52 +0700

    chore: add forgot password views

--- app/src/main/java/com/ichwan/rest/quranapi/auth/presentation/ForgotPasswordFragment.kt
+++ app/src/main/java/com/ichwan/rest/quranapi/auth/presentation/ForgotPasswordFragment.kt
@@ -5,16 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.ichwan.rest.quranapi.R
+import com.ichwan.rest.quranapi.databinding.FragmentForgotPasswordBinding
 
 class ForgotPasswordFragment : Fragment() {
+
+    private lateinit var binding: FragmentForgotPasswordBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forgot_password, container, false)
+    ): View {
+        binding = FragmentForgotPasswordBinding.inflate(layoutInflater)
+
+        binding.apply {
+            buttonChange.setOnClickListener {
+                findNavController().navigate(R.id.forgot_to_login)
+            }
+        }
+
+        return binding.root
     }
 
 }
